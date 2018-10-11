3fbbef0587f9a613d2e7bba30800d3eb915b7e1a
Alex <Alexiosdut@gmail.com>
2018-10-11 22:26:28 +0300

    Header, Movies and Footer components (CSS)

--- src/App.js
+++ src/App.js
@@ -2,14 +2,16 @@ import React, { Component } from "react";
 import "./App.css";
 import Header from "./components/Header";
 import Movies from "./components/Movies";
+import Footer from "./components/Footer";
 
 class App extends Component {
   render() {
     return (
-      <div>
+      <React.Fragment>
         <Header />
         <Movies />
-      </div>
+        <Footer />
+      </React.Fragment>
     );
   }
 }
---
+++ src/components/Footer.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer>
+      <div className="row">
+        <ul>
+          <li>
+            <a href="#">MYMDB</a>
+          </li>
+          <li>
+            <p>© Copyright all rights reserved 2018 — MYMDB</p>
+          </li>
+          <li>
+            <a
+              href="https://www.themoviedb.org/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              The movie db
+            </a>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
