ca60151aba473dafece402999aa71ffbec597480
Alex <Alexiosdut@gmail.com>
2018-10-14 00:27:08 +0300

    add genres, a heroMovie to state and implement getMovieGenres method in AppContext then use them to render needed data in Header component

--- src/AppContext.js
+++ src/AppContext.js
@@ -5,23 +5,49 @@ const AppContext = createContext();
 
 export class AppProvider extends Component {
   state = {
-    movies: []
+    movies: [],
+    heroMovie: {
+      genre_ids: []
+    },
+    genres: []
   };
 
   async componentDidMount() {
-    const res = await axios.get(
+    const genresRes = await axios.get(
+      `https://api.themoviedb.org/3/genre/movie/list?api_key=${
+        process.env.REACT_APP_API_KEY
+      }&language=en-US`
+    );
+
+    const genres = await genresRes.data.genres;
+
+    const moviesRes = await axios.get(
       `https://api.themoviedb.org/3/discover/movie?api_key=${
         process.env.REACT_APP_API_KEY
       }&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=false&page=1`
     );
-    const movies = await res.data.results;
+    const movies = await moviesRes.data.results;
 
-    this.setState({ movies });
+    this.setState({ ...this.state, movies, heroMovie: movies[0], genres });
   }
+
+  getMovieGenres = movie =>
+    [].concat.apply(
+      [],
+      movie.genre_ids.map(id =>
+        this.state.genres.filter(genre => genre.id === id && genre.name)
+      )
+    );
 
   render() {
     return (
-      <AppContext.Provider value={this.state}>
+      <AppContext.Provider
+        value={{
+          heroMovie: this.state.heroMovie,
+          movies: this.state.movies,
+          getMovieGenres: this.getMovieGenres
+        }}
+      >
         {this.props.children}
       </AppContext.Provider>
     );
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,64 @@
 import React, { Component } from "react";
+import { Consumer } from "../AppContext";
+import axios from "axios";
 
 class Header extends Component {
   render() {
-    return (
-      <header>
-        <nav>
-          <div className="nav-row">
-            <ul className="nav-bar">
-              <li>
-                <a href="#">MYMDB</a>
-              </li>
-              <li>
-                <a
-                  href="https://www.themoviedb.org/"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  The movie db
-                </a>
-              </li>
-            </ul>
-          </div>
-        </nav>
+    const backdrop_path_size = "http://image.tmdb.org/t/p/w1280";
 
-        <div className="header-content">
-          <div className="row">
-            <h1>The martian</h1>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima in
-              facere obcaecati sint sequi accusamus sed reiciendis maiores eum
-              hic?
-            </p>
-          </div>
-        </div>
-      </header>
+    return (
+      <Consumer>
+        {context => {
+          const { heroMovie, getMovieGenres } = context;
+
+          const movieGenres = getMovieGenres(heroMovie).map(genre => (
+            <span key={genre.id}>{genre.name}</span>
+          ));
+
+          return (
+            <header
+              style={{
+                backgroundImage:
+                  heroMovie.backdrop_path &&
+                  `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backdrop_path_size}${
+                    heroMovie.backdrop_path
+                  })`
+              }}
+            >
+              <nav>
+                <div className="nav-row">
+                  <ul className="nav-bar">
+                    <li>
+                      <a href="#">MYMDB</a>
+                    </li>
+                    <li>
+                      <a
+                        href="https://www.themoviedb.org/"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                      >
+                        The movie db
+                      </a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+
+              <div className="header-content">
+                <div className="row">
+                  <h1>{heroMovie.title}</h1>
+                  <p>{movieGenres}</p>
+                  <p>{heroMovie.overview}</p>
+                  <div className="release">
+                    <p>Release Date</p>
+                    <p>{heroMovie.release_date}</p>
+                  </div>
+                </div>
+              </div>
+            </header>
+          );
+        }}
+      </Consumer>
     );
   }
 }
