c5c760ea40a28ab42e1bbae2185c99e39e7a0485
Alex <Alexiosdut@gmail.com>
2018-10-14 04:25:28 +0300

    make movie title fit in one line with ellipsize module - Movie Component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,14 +1,16 @@
 import React from "react";
 import PropTypes from "prop-types";
+import ellipsize from "ellipsize";
 
 const Movie = ({ movie }) => {
   const poster_path_size = "http://image.tmdb.org/t/p/w300";
+  const movieTitle = ellipsize(movie.title, 20);
 
   return (
     <div className="movie-box">
       <img src={`${poster_path_size}${movie.poster_path}`} alt={movie.title} />
       <div>
-        <h3>{movie.title}</h3>
+        <h3>{movieTitle}</h3>
       </div>
     </div>
   );
