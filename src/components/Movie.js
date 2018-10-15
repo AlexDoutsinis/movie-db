9aa85460f6a89cae87efe46d35a44519290c9232
Alex <Alexiosdut@gmail.com>
2018-10-15 07:16:40 +0300

    added genres overview to each movie - Movie component

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,18 +1,37 @@
 import React from "react";
 import PropTypes from "prop-types";
 import ellipsize from "ellipsize";
+import { Consumer } from "../AppContext";
 
 const Movie = ({ movie }) => {
   const poster_path_size = "http://image.tmdb.org/t/p/w300";
   const movieTitle = ellipsize(movie.title, 20);
 
   return (
-    <div className="movie-box">
-      <img src={`${poster_path_size}${movie.poster_path}`} alt={movie.title} />
-      <div>
-        <h3>{movieTitle}</h3>
-      </div>
-    </div>
+    <Consumer>
+      {context => {
+        const { getMovieGenres } = context;
+
+        const movieGenres = getMovieGenres(movie).map(genre => (
+          <span key={genre.id}>{genre.name}</span>
+        ));
+
+        const genres = movieGenres.filter((item, index) => index < 3);
+
+        return (
+          <div className="movie-box">
+            <img
+              src={`${poster_path_size}${movie.poster_path}`}
+              alt={movie.title}
+            />
+            <div className="quick-infos">
+              <h3>{movieTitle}</h3>
+              <p>{genres}</p>
+            </div>
+          </div>
+        );
+      }}
+    </Consumer>
   );
 };
 
