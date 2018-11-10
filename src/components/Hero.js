11f4eccfe46c97c326e50deb561cbd1c5a4bf392
Alex <Alexiosdut@gmail.com>
2018-11-10 17:57:04 +0200

    improved responsivness for small screens

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -24,7 +24,7 @@ const Hero = () => (
           const bgImageStyles = {
             backgroundImage:
               movie.backdrop_path &&
-              `linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url(${backdrop_path_size}${
+              `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${backdrop_path_size}${
                 movie.backdrop_path
               })`,
             textDecoration: "none"
